f3b480fe2aa441bd7a7aa898c5d3956b76d6c991
VadymPaliushek <vadim.paliushek@gmail.com>
2022-04-18 16:40:46 -0400

    minting contract based on ERC721a

---
+++ hardhat.config.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:["963fa5d3b24fb00abf83cfb79670dd5e1262d3af486fcb063ea8fed5746534a3"]
+    },
+    bsctest:{
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts:["963fa5d3b24fb00abf83cfb79670dd5e1262d3af486fcb063ea8fed5746534a3"]
+    }
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: "2H9RY39J61XM4AB6VJH85I1HU4DTNQJP3J"
+  },
+};
